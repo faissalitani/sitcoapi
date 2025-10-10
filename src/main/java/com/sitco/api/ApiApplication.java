ebfa56878eec5894772ba6efb863de611182d8c6
Faissal Itani <f.itani@gmail.com>
2025-10-10 19:22:08 +0300

    initial commit

---
+++ src/main/java/com/sitco/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package com.sitco.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World with a change 2 minutes 30 seconds";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
